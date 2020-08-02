f46087a3a86f00473119440b527cd26872527311
Mariia Perepelkina <perepelkina.marija@gmail.com>
2020-08-02 13:57:10 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react'; 
+
+function App() {
+
+  const [color, setColor] = useState('#d6909c')
+
+  function changeColor() {
+    setColor('#000')
+  
+  }
+  
+  return (
+    <div className='wrapper'>
+      <h1>Homework</h1>
+      <button className = 'button' onClick={changeColor}> 
+        Press me!
+      </button>
+    <div className = 'square'>{color}</div>
+    </div>
+  )
+  }
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+import { useState } from 'react' 
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+    function decrementCount() {
+        setCount(count - 1)
+    }
+    function incrementCount() {
+        setCount(count + 1)
+    }
+
+    return(
+        <div className='wrapper'>
+            <button onClick={decrementCount}>Button -</button>
+            <span>{ count }</span>
+            <button onClick={incrementCount}>Button +</button>
+        </div>
+    )
+
+}
+
+export default Counter; 
